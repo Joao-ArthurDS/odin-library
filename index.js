59e6c584e02b1426a4dcd5ba52b232e73383d3f6
Joao <146687118+Joao-ArthurDS@users.noreply.github.com>
2026-07-18 18:00:17 -0300

    started cards styling and working on the js logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -6,28 +7,55 @@ function Book(title, author, pages, read){
     this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID();
+    
+    this.info =  function(){
+        const beenRead = this.read ? 'Book already read'  : 'Not read yet';
 
-    function info(){
-        const beenRead = 'book already read' ? this.read : 'not read yet';
-
-        `${this.title} by ${this.author}, ${this.pages} pages, ${beenRead}.`
+        return(`${this.title} by ${this.author}, ${this.pages} pages, ${beenRead}.`);
     }
-}
+};
 
 function addBookToLibrary(){
     const title = document.getElementById('title').value; 
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
-    const record = new Book(title, author, pages, false);
+    const read = document.getElementById('read').checked;
+    const record = new Book(title, author, pages, read);
 
     myLibrary.push(record);
     clearDialog();
     const bookForm = document.getElementById('my-dialog'); 
     bookForm.close();
-}
+    displayBooks();
+};
 
 function clearDialog(){
     const bookForm = document.getElementById('book_info'); 
     bookForm.reset();
         
-}
+};
+
+function displayBooks(){ 
+    for (var book of myLibrary){
+        if (!document.getElementById(book.id)){
+            const div = document.createElement('div');
+            div.classList.add('card');
+            div.setAttribute('id', book.id);
+            
+            const p1 = document.createElement('p');
+            const p2 = document.createElement('p');
+            const p3 = document.createElement('p');
+            
+            p1.innerHTML = book.title;
+            div.appendChild(p1);
+            
+            p2.innerHTML = `${book.author}, Total pages: ${book.pages}`;
+            div.appendChild(p2);
+
+            p3.innerHTML = book.info();
+            div.appendChild(p3);
+
+            body.appendChild(div);
+        }
+    }
+};
